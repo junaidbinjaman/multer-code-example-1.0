99d15f4de094f059ee15bea74b300ec4977c1f5e
iMac <imac@jwolts-iMac.local>
2021-11-29 07:48:33 +0600

    multyer code example

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const express = require("express");
 var multer = require("multer");
+const path = require("path");
 
 // file upload folder
 const UPLOAD_FOLDER = "uploads";
 
+// multer define stogare
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, UPLOAD_FOLDER);
+  },
+  filename: (req, file, cb) => {
+    const fileExt = path.extname(file.originalname);
+    const fileName =
+      file.originalname
+        .replace(fileExt, "")
+        .toLowerCase()
+        .split(" ")
+        .join("-") + "-" + Date.now();
+        cb(null, fileName + fileExt)
+  },
+});
+
 // multer works here
 const upload = multer({
-  dest: UPLOAD_FOLDER,
+  storage: storage,
   limits: {
     fileSize: 1000000,
   },
   fileFilter: (req, file, cb) => {
-    if(file.fieldname === 'avatar') {
+    if (file.fieldname === "avatar") {
       if (
         file.mimetype === "image/png" ||
         file.mimetype === "image/jpg" ||
@@ -21,18 +39,15 @@ const upload = multer({
       } else {
         cb(new Error("Only .jpg .png .jpeg format allowed!"));
       }
-    } else if (file.fieldname === 'doc') {
-      if (
-        file.mimetype === "application/pdf"
-      ) {
+    } else if (file.fieldname === "doc") {
+      if (file.mimetype === "application/pdf") {
         cb(null, true);
       } else {
         cb(new Error("Only .pdf format allowed!"));
       }
     } else {
-      cb(new Error("These was an unknown error"))
+      cb(new Error("These was an unknown error"));
     }
-    
   },
 });
 
@@ -45,6 +60,7 @@ app.post(
     { name: "doc", maxCount: 1 },
   ]),
   (req, res) => {
+    console.log(req.files);
     res.send("Hello, World");
   }
 );
